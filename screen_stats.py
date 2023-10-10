716db0cd19c446900e9be0a78de3fdee09dc11f0
Socvest <82335390+Socvest@users.noreply.github.com>
2023-10-10 02:23:21 +0100

    Update screen_stats.py

--- screen_stats.py
+++ screen_stats.py
@@ -2,5 +2,5 @@ import streamlit as st
 from st_screen_stats import st_screen_data
 st.set_page_config(layout="wide")
 
-
-st.write(st_screen_data())
+screen_data = st_screen_data()
+st.write(screen_data)
