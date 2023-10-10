fe8d93628438a40484f16c4c3031a16285476caf
Socvest <82335390+Socvest@users.noreply.github.com>
2023-10-10 13:52:52 +0100

    Update screen_stats.py

--- screen_stats.py
+++ screen_stats.py
@@ -1,6 +1,18 @@
 import streamlit as st
-from st_screen_stats import st_screen_data
-st.set_page_config(layout="wide")
+from st_screen_stats import ScreenData, StreamlitNativeWidgetScreen
 
-screen_data = st_screen_data()
-st.write(screen_data)
+
+st.subheader("Component with constant args")
+
+screenD = ScreenData()
+screen_d = screenD.st_screen_data_window_top()
+
+st.write(screen_d)
+
+
+st.subheader("native widget method")
+screenDN = StreamlitNativeWidgetScreen()
+screenDN.st_screen_data_window_top()
+stats_ = screenDN.get_window_screen_stats()
+st.write(stats_)
+
